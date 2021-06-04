196a1db0ff7b64d5445874f5229260c83e5f4d15
shruthi-git-actions <shruthia_k@hcl.com>
2021-06-04 23:34:52 +0530

    First commit with setup and DVC files

--- code/Train.py
+++ code/Train.py
@@ -23,100 +23,28 @@ import csv
 
 
 # In[ ]:
+
+'''resource_url = dvc.api.get_url(
+    'data/data.xml',
+    repo='git@github.com:shruthi-git-actions/dvc_v1.git'
+    )
+
+print(resource_url)
+'''
+
 with dvc.api.open(
         'train_data.csv',
         repo='git@github.com:shruthi-git-actions/dvc_v1.git',
-        remote='myremote'
+        remote='remote_storage'
         ) as fd:
     main_df=csv.reader(fd)
 #url="https://drive.google.com/file/d/1mzsSx_8EPDCLQ77Md4aOg6V9D-769tGP/view?usp=sharing"
 #path = 'https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
-main_df1 = pd.read_csv(main_df)
+#main_df1 = pd.read_csv(main_df)
 #main_test = dd.read_csv('test_data.csv')
 #url1="https://drive.google.com/file/d/1Xn8q8UwNgNlpjfMiSxn-vV5TzYjDiYRN/view?usp=sharing"
 #path1 = 'https://drive.google.com/uc?export=download&id='+url1.split('/')[-2]
-columns_req = pd.read_csv(data/column_list_new.csv)
-
-
-
-
-# In[ ]:
-
-main_dff = dd.from_pandas(main_df1, npartitions=7)
-df=main_dff[columns_req["Variable_list"]]
-#df_test=main_test[columns_req["Variable_list"]]
-no_columns=len(columns_req.index)
-
-
-# In[ ]:
-
-
-x=df.iloc[:,0:no_columns-1]
-y_train=df.iloc[:,-1]
-#test_x=df_test.iloc[:,0:no_columns-1]
-#test_train=df_test.iloc[:,-1]
-
-
-# In[ ]:
-
-
-x=x.categorize()
-
-
-# In[ ]:
-
-
-de = DummyEncoder()
-X_train = de.fit_transform(x)
-
-
-# In[ ]:
-
-
-client = Client(processes=False)             # create local cluster
-
-clf = RandomForestClassifier(n_estimators=200, n_jobs=-1)
-with joblib.parallel_backend("dask", scatter=[X_train, y_train]):
- clf.fit(X_train, y_train)
-
-
-# In[ ]:
-
-
-
-Pkl_Filename = "HumanEvent_Model.pkl"  
-
-with open(Pkl_Filename, 'wb') as file:  
-    pickle.dump(clf, file)
-
-
-# In[ ]:
-
-
-pred_train=clf.predict(X_train)
-
-
-# In[ ]:
-
-
-pred_train_df=dd.from_array(pred_train)
-pred_train_df=pred_train_df.to_frame()
-training=x.merge(pred_train_df)
-
-
-# In[ ]:
-
-
-
-
-training.to_csv("out/training_new.csv", single_file = True)
-
-
-# In[ ]:
-train_score=accuracy_score(np.array(y_train),np.array(pred_train))
-
-print(train_score)
-print ("the end ")
+#columns_req = pd.read_csv(data/column_list_new.csv)
 
 
 
