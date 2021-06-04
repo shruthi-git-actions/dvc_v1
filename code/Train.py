486b299b3951ca1df447dd0e1842da16933f99fc
shruthi-git-actions <shruthia_k@hcl.com>
2021-06-05 01:44:07 +0530

    Change SGD max_iter to 100

--- code/Train.py
+++ code/Train.py
@@ -23,29 +23,102 @@ import csv
 
 
 # In[ ]:
-
-'''resource_url = dvc.api.get_url(
-    'data/data.xml',
-    repo='git@github.com:shruthi-git-actions/dvc_v1.git'
-    )
-
-print(resource_url)
-'''
-
+#input
 with dvc.api.open(
-        'train_data.csv',
+        'data/train_data.csv',
         repo='https://github.com/shruthi-git-actions/dvc_v1.git',
         remote='remote_1',
-        rev="experiment"
+        rev="experiment",
+        encoding='utf-8'
         ) as fd:
-    main_df=csv.reader(fd)
-#url="https://drive.google.com/file/d/1mzsSx_8EPDCLQ77Md4aOg6V9D-769tGP/view?usp=sharing"
-#path = 'https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
-#main_df1 = pd.read_csv(main_df)
-#main_test = dd.read_csv('test_data.csv')
-#url1="https://drive.google.com/file/d/1Xn8q8UwNgNlpjfMiSxn-vV5TzYjDiYRN/view?usp=sharing"
-#path1 = 'https://drive.google.com/uc?export=download&id='+url1.split('/')[-2]
-#columns_req = pd.read_csv(data/column_list_new.csv)
+    main_df=pd.read_csv(fd)
+#data
+with dvc.api.open(
+        'data/column_list_new.csv',
+        repo='https://github.com/shruthi-git-actions/dvc_v1.git',
+        remote='remote_1',
+        rev="experiment",
+        encoding='utf-8'
+        ) as fd1:
+    columns_req=pd.read_csv(fd1)
+
+#Training
+main_dff = dd.from_pandas(main_df, npartitions=7)
+df=main_dff[columns_req["Variable_list"]]
+#df_test=main_test[columns_req["Variable_list"]]
+no_columns=len(columns_req.index)
+
+
+# In[ ]:
+
+
+x=df.iloc[:,0:no_columns-1]
+y_train=df.iloc[:,-1]
+#test_x=df_test.iloc[:,0:no_columns-1]
+#test_train=df_test.iloc[:,-1]
+
+
+# In[ ]:
+
+
+x=x.categorize()
+
+
+# In[ ]:
+
+
+de = DummyEncoder()
+X_train = de.fit_transform(x)
+
+
+# In[ ]:
+
+
+client = Client(processes=False)             # create local cluster
+
+clf = RandomForestClassifier(n_estimators=200, n_jobs=-1)
+with joblib.parallel_backend("dask", scatter=[X_train, y_train]):
+ clf.fit(X_train, y_train)
+
+
+# In[ ]:
+
+
+
+Pkl_Filename = "HumanEvent_Model.pkl"  
+
+with open(Pkl_Filename, 'wb') as file:  
+    pickle.dump(clf, file)
+
+
+# In[ ]:
+
+
+pred_train=clf.predict(X_train)
+
+
+# In[ ]:
+
+
+pred_train_df=dd.from_array(pred_train)
+pred_train_df=pred_train_df.to_frame()
+training=x.merge(pred_train_df)
+
+
+# In[ ]:
+
+
+
+
+training.to_csv("training_new.csv", single_file = True)
+
+
+# In[ ]:
+train_score=accuracy_score(np.array(y_train),np.array(pred_train))
+
+print(train_score)
+print ("the end ")
+ 
 
 
 
